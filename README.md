cb033f0fd59f5c617c751708e37e0fae8c6106ac
Tom Artiom Fiodorov <tom@glassnode.com>
2020-03-15 12:21:45 +0000

    using flats

--- README.md
+++ README.md
@@ -9,16 +9,20 @@ Currently I used this for generating major scales:
 > cargo run > /tmp/music.txt
 
 # outputs:
-notes in A major; A B C# D E F# G#
-notes in A# major; A# C D D# F G A
-notes in B major; B C# D# E F# G# A#
-notes in C major; C D E F G A B
-notes in C# major; C# D# F F# G# A# C
-notes in D major; D E F# G A B C#
-notes in D# major; D# F G G# A# C D
-notes in E major; E F# G# A B C# D#
-notes in F major; F G A A# C D E
-notes in F# major; F# G# A# B C# D# F
-notes in G major; G A B C D E F#
-notes in G# major; G# A# C C# D# F G
+3 enharmonics; C# F# B# <-> Db Gb Cb
+major scale of C; F C G D A E B
+major scale of G; C G D A E B F#
+major scale of D; G D A E B F# C#
+major scale of A; D A E B F# C# G#
+major scale of E; A E B F# C# G# D#
+major scale of Ab; Db Ab Eb Bb F C G
+major scale of Eb; Ab Eb Bb F C G D
+major scale of Bb; Eb Bb F C G D A
+major scale of F; A# F C G D A E
+major scale of Db; (<-> C#) Gb Db Ab Eb Bb F C
+major scale of Gb; (<-> F#) Cb Gb Db Ab Eb Bb F
+major scale of Cb; (<-> B) E Cb Gb Db Ab Eb Bb
+major scale of C#; (<-> Db) F# C# G# D# A# F C
+major scale of F#; (<-> Gb) B F# C# G# D# A# F
+major scale of B; (<-> Cb) E B F# C# G# D# A#
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,90 @@
 use std::convert::TryFrom;
 
-static NOTES: [&'static str; 12] = [
+static SHARP_NOTES: [&'static str; 12] = [
     "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
 ];
 
-fn frequency_to_note(frequency: f64) -> &'static str {
+static FLAT_NOTES: [&'static str; 12] = [
+    "A", "Bb", "Cb", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
+];
+
+fn add_semetones(frequency: f64, n: i64) -> f64 {
+    frequency * 2f64.powf(n as f64 / 12.0)
+}
+
+fn frequency_to_note(frequency: f64, flat: bool) -> &'static str {
     let num_semisteps = 12.0 * (frequency / 440.0).log2();
     let num_semisteps = num_semisteps.round() as i64;
     let num_semisteps = num_semisteps.rem_euclid(12);
     let index = usize::try_from(num_semisteps).unwrap();
-    NOTES[index]
+    if flat {
+        return FLAT_NOTES[index];
+    }
+    SHARP_NOTES[index]
+}
+
+fn is_flat(note: &str) -> bool {
+    let ch = note.chars().last().unwrap();
+    ch == 'b'
 }
 
 fn note_to_frequency(note: &str) -> f64 {
-    let pos = NOTES.iter().position(|x| x == &note).unwrap() as f64;
-    440.0 * 2.0f64.powf(pos / 12.0)
+    if let Some(p) = SHARP_NOTES.iter().position(|x| x == &note) {
+        return 440.0 * 2.0f64.powf(p as f64 / 12.0);
+    }
+    let p = FLAT_NOTES.iter().position(|x| x == &note).unwrap();
+    440.0 * 2.0f64.powf(p as f64 / 12.0)
+}
+
+fn perfect_n_fifth(note: &str, n: i64) -> &'static str {
+    let start = note_to_frequency(note);
+    frequency_to_note(add_semetones(start, 7 * n), is_flat(note))
+}
+
+fn perfect_fifth(note: &str) -> &'static str {
+    perfect_n_fifth(note, 1)
+}
+
+fn perfect_n_forths(note: &str, n: i64) -> &'static str {
+    let start = note_to_frequency(note);
+    frequency_to_note(add_semetones(start, 5 * n), is_flat(note))
+}
+
+fn perfect_forth(note: &str) -> &'static str {
+    perfect_n_forths(note, 1)
 }
 
 fn major_scale(note: &str) -> Vec<&'static str> {
-    let scale = vec![0, 2, 4, 5, 7, 9, 11];
-    let start = note_to_frequency(note);
-    let v: Vec<_> = scale
-        .iter()
-        .map(|s| {
-            let s = *s as f64;
-            frequency_to_note(start * 2f64.powf(s / 12.0))
-        })
-        .collect();
-    v
+    let scale = vec![-1, 0, 1, 2, 3, 4, 5];
+    scale.iter().map(|s| perfect_n_fifth(note, *s)).collect()
+}
+
+fn enharmonic(note: &str) -> &'static str {
+    frequency_to_note(note_to_frequency(note), !is_flat(note))
 }
 
 fn main() {
-    for note in &NOTES {
-        println!("notes in {} major; {}", note, major_scale(note).join(" "));
+    println!("3 enharmonics; C# F# B# <-> Db Gb Cb");
+    for note in &["C", "G", "D", "A", "E"] {
+        println!("major scale of {}; {}", note, major_scale(note).join(" "))
+    }
+    for note in &["Ab", "Eb", "Bb", "F"] {
+        println!("major scale of {}; {}", note, major_scale(note).join(" "))
+    }
+    for note in &["Db", "Gb", "Cb"] {
+        println!(
+            "major scale of {}; (<-> {}) {}",
+            note,
+            enharmonic(note),
+            major_scale(note).join(" ")
+        )
+    }
+    for note in &["C#", "F#", "B"] {
+        println!(
+            "major scale of {}; (<-> {}) {}",
+            note,
+            enharmonic(note),
+            major_scale(note).join(" ")
+        )
     }
 }
